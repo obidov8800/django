cc33e31c54589581b5ade1d7b636ba4b95e66641
obidov8800 <110780827+obidov8800@users.noreply.github.com>
2025-05-29 10:26:44 +0500

    Update settings.py

--- student_test_platform_project/settings.py
+++ student_test_platform_project/settings.py
@@ -30,7 +30,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 SECRET_KEY = 'django-insecure-n1+d7h2*3+-ob63*jxzqqi#gzk*4&#x3d&3=muidb02z+8-84^'
 
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['student-tests.onrender.com', 'localhost', '127.0.0.1']
 
